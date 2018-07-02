3824c146e0242ac6115e3a9d1dc95d4e1ab68c4d
chopins <chopins.xiao@gmail.com>
2018-07-02 16:51:26 +0800

    update

--- src/CommandLine.php
+++ src/CommandLine.php
@@ -10,23 +10,31 @@
 
 namespace Toknot\Output;
 
-use Toknot\Output\Printer;
-use Toknot\Input\CommandInput;
-use Toknot\Misc\{Integer,Boolean,Char};
 use Toknot\Digital\Number;
+use Toknot\Input\CommandInput;
+use Toknot\Misc\Boolean;
+use Toknot\Misc\Char;
+use Toknot\Misc\Integer;
+use Toknot\Output\Printer;
 
-class CommandLine {
+class CommandLine
+{
 
-    private static $readline = false;
-    private static $ins = null;
+    private static $readline         = false;
+    private static $ins              = null;
+    public static $progMask          = '=';
+    public static $tobeMask          = '-';
+    public static $helpColumnSpacing = 8;
 
-    public function __construct() {
-        if (extension_loaded('readline')) {
+    public function __construct()
+    {
+        if (!self::$readline && extension_loaded('readline')) {
             self::$readline = true;
         }
     }
 
-    public static function __callStatic($name, $argvs = []) {
+    public static function __callStatic($name, $argvs = [])
+    {
         if (self::$ins === null) {
             self::$ins = new static();
         }
@@ -37,21 +45,24 @@ class CommandLine {
         throw new Exception("method $name exists in " . get_called_class());
     }
 
-    public function cli(): bool {
+    public function cli(): bool
+    {
         return PHP_SAPI === 'cli';
     }
 
-    public function readline() {
+    public function readlineState()
+    {
         return self::$readline;
     }
 
     /**
      * get terminal number of columns
-     * 
+     *
      * @param int $defaultCols
      * @return int
      */
-    public function getCols($defaultCols = 150) {
+    public function getCols($defaultCols = 150)
+    {
         $cols = $this->tput('cols');
         if (empty($cols)) {
             $size = trim(shell_exec('stty size'));
@@ -65,14 +76,16 @@ class CommandLine {
         return $cols;
     }
 
-    public function getShell() {
+    public function getShell()
+    {
         if (isset($_SERVER['SHELL'])) {
             return $_SERVER['SHELL'];
         }
         return 'unknow';
     }
 
-    public function newLine() {
+    public function newLine()
+    {
         if (self::$readline) {
             readline_on_new_line();
         } else {
@@ -80,7 +93,8 @@ class CommandLine {
         }
     }
 
-    public function read($prompt = '', $color = 0) {
+    public function read($prompt = '', $color = 0)
+    {
         if (self::$readline) {
             $prompt = Printer::addCLIColor($prompt, $color);
             return readline($prompt);
@@ -91,7 +105,8 @@ class CommandLine {
         }
     }
 
-    public function flushLine($msg, $color) {
+    public function flushLine($msg, $color)
+    {
         if (is_array($msg)) {
             $linenum = count($msg);
             $this->tput('cuu', $linenum, 0);
@@ -106,34 +121,36 @@ class CommandLine {
 
     /**
      * exec interactive shell
-     * 
+     *
      * @param callable $callable    callable after input
      * @param string $prompt        shell prompt message
      */
-    public function interactive(array $prompts, array $optionValue, $color = 0) {
+    public function interactive(array $prompts, array $optionValue, $color = 0)
+    {
         $colorArr = is_array($color);
         foreach ($prompts as $i => $p) {
-            if(is_array($p)) {
+            if (is_array($p)) {
                 $default = $p[1];
-                $prompt = $p[0];
+                $prompt  = $p[0];
             } else {
                 $default = null;
-                $prompt = $p;
+                $prompt  = $p;
             }
             $this->checkInput($prompt, $optionValue[$i], $default, $colorArr ? $color[$i] : $color);
         }
     }
 
     /**
-     * 
+     *
      * @param string $prompt
      * @param array|string $optionValue
      * @param type $default
      * @param type $color
      * @return type
      */
-    public function checkInput($prompt, $optionValue = [], $default = null, $color = 0) {
-        if(is_array($optionValue)) {
+    public function checkInput($prompt, $optionValue = [], $default = null, $color = 0)
+    {
+        if (is_array($optionValue)) {
             $prompt .= '(' . implode('/', $optionValue) . ')';
         }
         do {
@@ -149,21 +166,96 @@ class CommandLine {
                 return $input;
             } elseif ($optionValue === Number && is_numeric($input)) {
                 return $input;
-            } elseif($optionValue instanceof Char) {
+            } elseif ($optionValue instanceof Char) {
                 return $input;
-            } elseif($optionValue === $input) {
+            } elseif ($optionValue === $input) {
                 return $input;
             }
         } while (true);
     }
 
-    protected function printColorLine($msg, $color, $last = '') {
-        $msg = str_pad($msg, $this->getCols(), ' ');
+    public function setBgProgMask()
+    {
+        self::$progMask = Printer::addCLIColor(' ', Printer::COLOR_B_WHITE);
+    }
+
+    public function progress($percent, string $prompt, string $lastMsg)
+    {
+        if (\is_numeric($percent)) {
+            throw new Exception("progress percent must between 0 and 100 number each");
+        }
+        $percent     = \round($percent, 2);
+        $prolen      = $this->getCols() - strlen($prompt . $lastMsg . $percent) - 5;
+        $progCharLen = round($prolen / 100, 2);
+        $cur         = ceil($progCharLen * $percent);
+
+        $will     = $prolen - $cur;
+        $progMask = self::$progMask;
+        $tobeMask = self::$tobeMask;
+        $msg      = $prompt . sprintf(" [%'{$progMask}{$cur}s%'{$tobeMask}{$will}s] {$percent}%", $progMask, '') . $lastMsg;
+        $this->flushLine($msg);
+    }
+
+    public function showHelp(array $options, $color = null)
+    {
+        $max = [];
+        $i   = 0;
+        foreach ($options as $i => $row) {
+            if ($i !== 0 && \is_array($row)) {
+                $i++;
+                continue;
+            }
+            $len     = strlen($row[0]);
+            $max[$i] = $len > $max ? $len : $max;
+        }
+        $k = 0;
+        foreach ($options as $row) {
+            if (\is_scalar($row)) {
+                Printer::printn($row);
+                $k++;
+            } elseif (count($row) > 2) {
+                $desc = $row[1];
+                unset($row[1]);
+                $msg = join(', ', $row);
+                Printer::printn($msg);
+                $spaceLen   = $max[$k] + self::$helpColumnSpacing;
+                $this->printOptionDesc($desc, $spaceLen);
+            } elseif(count($row) == 1) {
+                Printer::printn($row[0]);
+            } else {
+                $space = $max[$k] - strlen($row[0]) + self::$helpColumnSpacing;
+                $msg   = sprintf("{$row[0]}%-{$space}s", '');
+                Printer::put($msg);
+                $optLen  = strlen($msg);
+                $this->printOptionDesc($row[1], $optLen, $pad);
+            }
+        }
+    }
+
+    protected function printOptionDesc($optDesc, $optLen)
+    {
+        $descLen = $this->getCols() - $optLen;
+        $pad     = sprintf("%{$optLen}s", '');
+        $descOffset = 0;
+        while (true) {
+            $desc = substr($optDesc, $descOffset, $descLen);
+            if (!$desc) {
+                break;
+            }
+            $descOffset += $deslen;
+            Printer::printn($pad . $desc);
+        }
+    }
+
+    protected function printColorLine($msg, $color, $last = '')
+    {
+        $msg      = str_pad($msg, $this->getCols(), ' ');
         $colorMsg = Printer::addCLIColor($msg, $color) . $last;
         Printer::printn($colorMsg);
     }
 
-    protected function tput(...$argv) {
+    protected function tput(...$argv)
+    {
         return trim(shell_exec("tput " . implode(' ', $argv)));
     }
 
